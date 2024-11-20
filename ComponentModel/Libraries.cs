46359bc8c895758bf6f007ed7661ebddf1ee8b32
Lamparter <notlamparter@outlook.com>
2024-11-20 18:45:51 +0000

    Add library enumeration

---
+++ ComponentModel/Libraries.cs
@@ -0,0 +1,33 @@
+﻿namespace Riverside.ComponentModel
+{
+    public static class Libraries
+    {
+        public const string Shell32 = "shell32.dll";
+        public const string Kernel32 = "kernel32.dll";
+        public const string User32 = "user32.dll";
+        public const string AdvancedWin32 = "advapi32.dll";
+        public const string Gdi32 = "gdi32.dll";
+        public const string Ole32 = "ole32.dll";
+        public const string Ws2_32 = "ws2_32.dll";
+        public const string Crypt32 = "crypt32.dll";
+        public const string Msvcrt = "msvcrt.dll";
+        public const string Comdlg32 = "comdlg32.dll";
+        public const string Shlwapi = "shlwapi.dll";
+        public const string Winspool = "winspool.drv";
+        public const string Netapi32 = "netapi32.dll";
+        public const string Iphlpapi = "iphlpapi.dll";
+        public const string Psapi = "psapi.dll";
+        public const string Dbghelp = "dbghelp.dll";
+        public const string Version = "version.dll";
+        public const string Ntdll = "ntdll.dll";
+        public const string Mpr = "mpr.dll";
+        public const string Wininet = "wininet.dll";
+        public const string Wldap32 = "wldap32.dll";
+        public const string Urlmon = "urlmon.dll";
+        public const string Setupapi = "setupapi.dll";
+        public const string Ws2help = "ws2help.dll";
+        public const string Wshtcpip = "wshtcpip.dll";
+        public const string RpcRt4 = "rpcrt4.dll";
+        public const string Mswsock = "mswsock.dll";
+    }
+}
