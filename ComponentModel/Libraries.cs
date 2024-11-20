2599f335f3d3a3a9c265b02cac500b2629f035e3
Lamparter <notlamparter@outlook.com>
2024-11-20 18:47:07 +0000

    Add more libraries

--- ComponentModel/Libraries.cs
+++ ComponentModel/Libraries.cs
@@ -5,7 +5,7 @@
         public const string Shell32 = "shell32.dll";
         public const string Kernel32 = "kernel32.dll";
         public const string User32 = "user32.dll";
-        public const string AdvancedWin32 = "advapi32.dll";
+        public const string Advapi32 = "advapi32.dll";
         public const string Gdi32 = "gdi32.dll";
         public const string Ole32 = "ole32.dll";
         public const string Ws2_32 = "ws2_32.dll";
@@ -13,7 +13,6 @@
         public const string Msvcrt = "msvcrt.dll";
         public const string Comdlg32 = "comdlg32.dll";
         public const string Shlwapi = "shlwapi.dll";
-        public const string Winspool = "winspool.drv";
         public const string Netapi32 = "netapi32.dll";
         public const string Iphlpapi = "iphlpapi.dll";
         public const string Psapi = "psapi.dll";
@@ -29,5 +28,29 @@
         public const string Wshtcpip = "wshtcpip.dll";
         public const string RpcRt4 = "rpcrt4.dll";
         public const string Mswsock = "mswsock.dll";
+        public const string Msvcp140 = "msvcp140.dll";
+        public const string Mfc140u = "mfc140u.dll";
+        public const string Msvcp120 = "msvcp120.dll";
+        public const string Dwmapi = "dwmapi.dll";
+        public const string D3d11 = "d3d11.dll";
+        public const string D3d12 = "d3d12.dll";
+        public const string D3d9 = "d3d9.dll";
+        public const string Dxgi = "dxgi.dll";
+        public const string Winmm = "winmm.dll";
+        public const string Imm32 = "imm32.dll";
+        public const string Opengl32 = "opengl32.dll";
+        public const string Glu32 = "glu32.dll";
+        public const string Opencl = "OpenCL.dll";
+        public const string Vcruntime140 = "vcruntime140.dll";
+        public const string Xinput1_4 = "xinput1_4.dll";
+        public const string Xinput9_1_0 = "xinput9_1_0.dll";
+        public const string Xinput1_3 = "xinput1_3.dll";
+        public const string Dsound = "dsound.dll";
+        public const string Dinput8 = "dinput8.dll";
+        public const string Mfplat = "mfplat.dll";
+        public const string Mfreadwrite = "mfreadwrite.dll";
+        public const string Mfuuid = "mfuuid.dll";
+        public const string AudioSes = "AudioSes.dll";
+        public const string Avrt = "avrt.dll";
     }
 }
